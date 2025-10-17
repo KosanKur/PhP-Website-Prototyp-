3113497a93af6a00b3d6603857fd57167e5ec568
KosanKur <133021531+KosanKur@users.noreply.github.com>
2025-10-17 22:53:07 +0200

    Add files via upload

---
+++ inculde/navigation.inc.php
@@ -0,0 +1,16 @@
+<header>
+<nav>
+    <div>
+<a href="index.php">
+    <img src="./images/sidequestlogo.png" alt="logo" loading="lazy">
+    <h2>Sidequest</h2>
+</a>
+    </div>
+    <div>
+
+    </div>
+
+
+</nav>
+
+</header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+    $titel= "Sidequest";
+    include './inculde/head.inc.php';
+    ?>
+    
+<body>
+  <?php
+
+include './inculde/navigation.inc.php';
+
+   ?>  
+
+</body>
+</html>
